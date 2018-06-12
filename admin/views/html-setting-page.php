2c029d7f78eb02eacc70beb942f742550729c9a0
ocws <ocws0115@gmail.com>
2018-06-12 21:52:10 +0900

    delete form tag temporarily

--- admin/views/html-setting-page.php
+++ admin/views/html-setting-page.php
@@ -9,6 +9,4 @@
 ?>
 <div class="wrap">
 	<h2><?php _e( 'Combined Calendar Setting', 'combined-calendar' ); ?></h2>
-		<from method="post" action="options.php" novalidate="novalidate">
-		</from>
 </div>
